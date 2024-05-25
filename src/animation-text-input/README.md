063123c256e3601d12d7f5eef80fe937001d9908
chitraket <chitraketsavani@gmail.com>
2024-05-25 23:51:55 +0530

    animation text input fix

---
+++ src/animation-text-input/README.md
@@ -0,0 +1,3 @@
+# React Native Animation Text Input
+
+![Demo](https://github.com/chitraket/animation/blob/main/src/animation-text-input/demo.gif)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import AnimationTextInput from './animation-text-input/animation-text-input';
 import AnimationTopTab from './animation-top-tab/animation-top-tab';
 
-export {AnimationTopTab};
+export {AnimationTopTab, AnimationTextInput};
