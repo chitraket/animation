be18d10485d3fe52e92641e3d847aca0d97b4f54
chitraket <chitraketsavani@gmail.com>
2024-06-30 23:56:32 +0530

    animation switch

---
+++ src/animation-switch/README.md
@@ -0,0 +1,3 @@
+# React Native Animation Switch
+
+![Demo](https://github.com/chitraket/animation/blob/main/src/animation-switch/demo.gif)
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import AnimationSwitch from './animation-switch/animation-switch';
 import AnimationTextInput from './animation-text-input/animation-text-input';
 import AnimationToast from './animation-toast/animation-toast';
 import AnimationTopTab from './animation-top-tab/animation-top-tab';
 
-
-export {AnimationTopTab, AnimationTextInput, AnimationToast};
+export {AnimationTopTab, AnimationTextInput, AnimationToast, AnimationSwitch};
